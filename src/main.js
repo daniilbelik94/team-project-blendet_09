3ceda5f42bf9cdee09fda9c3ddc5526cb56280ca
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-11-19 14:21:54 +0100

    initial commit
    Созданы папки, добавлены необходимые нпм и скачаны все нужные картинки

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './js/change-theme.js';
+import './js/mobile-menu.js';
+import './js/render-projects.js';
+import './js/reviews.js';
+import './js/form.js';
+import './js/accordion.js';
+import './js/scroll-up.js';
+import './js/title-animation.js';
+
+
+
+
