035b1963316e1475f8dbcc1fdb0ebc3e8ca93ca1
daniilbelik94 <161323222+daniilbelik94@users.noreply.github.com>
2024-11-20 11:47:06 +0100

    title-animation and section work-together
    добавить npm install split-type
    анимированы заголовет.
    добавлена секция работы вместе с формой и отпаравкой на сервер

---
+++ src/js/title-animation.js
@@ -0,0 +1,50 @@
+'use strict';
+
+import SplitType from 'split-type';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
+
+document.addEventListener('DOMContentLoaded', e => {
+  // Split text into spans
+  let typeSplit = new SplitType('.text-split', {
+    types: 'words, chars',
+    tagName: 'span',
+  });
+
+  // Link timelines to scroll position
+  const createScrollTrigger = (triggerElement, timeline) => {
+    // Reset tl when scroll out of view past bottom of screen
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: 'top bottom',
+      onLeaveBack: () => {
+        timeline.progress(0);
+        timeline.pause();
+      },
+    });
+    // Play tl when scrolled into view (60% from top of screen)
+    ScrollTrigger.create({
+      trigger: triggerElement,
+      start: 'top 90%',
+      onEnter: () => timeline.play(),
+    });
+  };
+
+  document
+    .querySelectorAll('.letters-fade-in-random')
+    .forEach((element, index) => {
+      let tl = gsap.timeline({ paused: true });
+      tl.from(element.querySelectorAll('.char'), {
+        opacity: 0,
+        duration: 0.5, // Увеличил продолжительность для более плавного появления
+        ease: 'expo.out', // Изменил на более мягкий "expo.out"
+        stagger: { amount: 1.2, from: 'start' }, // Немного увеличил time stagger
+      });
+      createScrollTrigger(element, tl);
+    });
+
+  // Avoid flash of unstyled content
+  gsap.set('.text-split', { opacity: 1 });
+});
